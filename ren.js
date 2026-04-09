20e30225b14a8c0eb3a8483a6db1c0574aaf3e44
arnold77-fc <arnoldclasic6@gmail.com>
2026-04-09 23:02:14 +0300

    Update ren.js

--- ren.js
+++ ren.js
@@ -1,97 +1,112 @@
 (function () {
     'use strict';
 
-    // 1. Константы и API ключи из файла
+    // Константы из вашего файла
     var OMDB_API_KEYS = ['73ff4450'];
     var KP_API_KEYS = ['5178ab83-699c-4422-937e-f8a759f872ef'];
-    var WEIGHTS = { imdb: 0.35, tmdb: 0.15, kp: 0.20, mc: 0.15, rt: 0.15 };
 
-    // 2. SVG Иконка для меню
-    var icon_star = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" fill="currentColor"/></svg>';
+    // 1. Создаем компонент настроек (само окно)
+    function RatingsSettings(object) {
+        var comp = new Lampa.InteractionMain(object);
 
-    // 3. Функция создания настроек
-    function setupSettings() {
-        // Добавляем новый пункт в главный список настроек
-        Lampa.Settings.add({
-            title: 'Рейтинг и качество',
-            type: 'book',
-            icon: icon_star,
-            name: 'maxsm_ratings_settings'
-        });
+        comp.create = function () {
+            this.activity.loader(false);
+            this.render().find('.interaction-main__body').append(this.build());
+        };
 
-        // Создаем содержимое этого пункта
-        Lampa.Settings.bind('maxsm_ratings_settings', function (item) {
-            item.append(Lampa.Template.get('settings_list_top', {
-                title: 'Настройки рейтингов',
-                descr: 'Управление отображением оценок и качества контента'
-            }));
+        comp.build = function () {
+            var _this = this;
+            var items = [
+                {
+                    title: 'Средний рейтинг',
+                    descr: 'Показывать вычисленный средний балл на основе всех сервисов',
+                    type: 'bool',
+                    name: 'ratings_show_avg',
+                    value: true
+                },
+                {
+                    title: 'Рейтинг Кинопоиск',
+                    descr: 'Отображать оценку с Кинопоиска',
+                    type: 'bool',
+                    name: 'ratings_show_kp',
+                    value: true
+                },
+                {
+                    title: 'Ключ Кинопоиск',
+                    descr: 'Ваш личный токен (по умолчанию встроенный)',
+                    type: 'input',
+                    name: 'ratings_kp_key',
+                    placeholder: 'Введите токен'
+                }
+            ];
 
-            // Переключатель отображения среднего рейтинга
-            var show_avg = Lampa.Template.get('settings_field', {
-                name: 'ratings_show_avg',
-                title: 'Средний рейтинг',
-                descr: 'Показывать вычисленный средний балл'
-            });
-            show_avg.append(Lampa.Template.get('settings_field_checkbox', { name: 'ratings_show_avg', value: true }));
-            item.append(show_avg);
+            var list = $('<div class="settings-list"></div>');
 
-            // Переключатель рейтинга Кинопоиска
-            var show_kp = Lampa.Template.get('settings_field', {
-                name: 'ratings_show_kp',
-                title: 'Рейтинг Кинопоиск',
-                descr: 'Отображать данные из KP API'
-            });
-            show_kp.append(Lampa.Template.get('settings_field_checkbox', { name: 'ratings_show_kp', value: true }));
-            item.append(show_kp);
+            items.forEach(function (item) {
+                var field = Lampa.Template.get('settings_field', item);
+                
+                if (item.type === 'bool') {
+                    var sw = Lampa.Template.get('settings_field_checkbox', {
+                        name: item.name,
+                        value: Lampa.Storage.get(item.name, item.value)
+                    });
+                    field.append(sw);
+                }
 
-            // Поле для ключа API (если пользователь захочет свой)
-            var kp_key = Lampa.Template.get('settings_field', {
-                name: 'ratings_kp_key',
-                title: 'Ключ Кинопоиск (опционально)',
-                descr: 'Ваш личный токен для API Кинопоиска'
-            });
-            kp_key.append('<div class="settings-param__value">Изменить</div>');
-            kp_key.on('hover:enter', function() {
-                Lampa.Input.edit({
-                    value: Lampa.Storage.get('ratings_kp_key', ''),
-                    free: true
-                }, function(new_value) {
-                    Lampa.Storage.set('ratings_kp_key', new_value);
-                    Lampa.Noty.show('Сохранено');
+                field.on('hover:enter', function () {
+                    if (item.type === 'bool') {
+                        var val = !Lampa.Storage.get(item.name, item.value);
+                        Lampa.Storage.set(item.name, val);
+                        field.find('.settings-param__checkbox').toggleClass('active', val);
+                    } else if (item.type === 'input') {
+                        Lampa.Input.edit({
+                            value: Lampa.Storage.get(item.name, ''),
+                            free: true
+                        }, function (new_val) {
+                            Lampa.Storage.set(item.name, new_val);
+                            Lampa.Noty.show('Сохранено');
+                        });
+                    }
                 });
+
+                list.append(field);
             });
-            item.append(kp_key);
-        });
+
+            return list;
+        };
+
+        return comp;
     }
 
-    // 4. Логика отображения в карточке
-    function initRatingsInCard() {
+    // 2. Регистрация в меню и системе
+    function init() {
+        // Регистрируем компонент в системе Lampa
+        Lampa.Component.add('ratings_gold_settings', RatingsSettings);
+
+        // Добавляем пункт в Настройки
+        Lampa.SettingsApi.addPart({
+            component: 'ratings_gold_settings',
+            name: 'ratings_gold_part',
+            tab: true, // Важно для Android версии
+            icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>',
+            title: 'Рейтинг и качество',
+            descr: 'Настройка внешних рейтингов и меток качества'
+        });
+
+        // Логика отображения в карточке
         Lampa.Listener.follow('full', function (e) {
             if (e.type !== 'complite') return;
-            
             var render = e.object.activity.render();
-            var ratingsCont = $('<div class="applecation__ratings show"></div>');
-            
-            // Если включено в настройках — показываем
-            if (Lampa.Storage.field('ratings_show_avg')) {
+            if (Lampa.Storage.get('ratings_show_avg', true)) {
                 var val = e.data.movie.vote_average || '0.0';
-                ratingsCont.append('<div class="rating-item" style="color:#FFDF6D; font-weight:bold; margin-right:15px;">★ '+val+'</div>');
+                var html = $('<div class="applecation__ratings show" style="display:flex; margin-top:10px; font-size:1.4em; font-weight:bold; color:#FFDF6D;">★ ' + val + '</div>');
+                render.find('.full-start__details').after(html);
             }
-            
-            render.find('.full-start__details').after(ratingsCont);
         });
     }
 
-    // 5. Запуск
-    function startPlugin() {
-        if (window.ratings_plugin_loaded) return;
-        window.ratings_plugin_loaded = true;
-
-        setupSettings();
-        initRatingsInCard();
-    }
-
-    if (window.appready) startPlugin();
-    else Lampa.Listener.follow('app', function (e) { if (e.type === 'ready') startPlugin(); });
+    // Запуск плагина
+    if (window.appready) init();
+    else Lampa.Listener.follow('app', function (e) { if (e.type === 'ready') init(); });
 
 })();
